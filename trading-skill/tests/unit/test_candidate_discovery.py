cbeb13fa0363198b8204bce8a7fc55320754508a
WJAnnie <50095919+WJAnnie@users.noreply.github.com>
2026-09-10 01:11:22 +0800

    test(trading-skill): lock fund asset class and liquidity representative rules

--- trading-skill/tests/unit/test_candidate_discovery.py
+++ trading-skill/tests/unit/test_candidate_discovery.py
@@ -6,6 +6,7 @@ from trading_skill.candidate_discovery import (
     classify_fund_category,
     discover_stock_candidates,
     finalize_candidates,
+    fund_risk_tags,
 )
 from trading_skill.market_universe import DataQuality, MarketSecurity, SecurityType, StockBoard
 
@@ -86,6 +87,20 @@ def test_same_etf_family_keeps_one_more_tradeable_representative():
     assert all(CandidateRoute.FUND_RELATIVE.value in x.source_routes for x in out)
 
 
+def test_same_family_prefers_liquidity_even_when_weaker_product_has_more_momentum():
+    funds = [
+        sec("510300", name="沪深300ETF", security_type=SecurityType.ETF, amount=900_000_000, ch60=5, day=0.2),
+        sec("510310", name="沪深300ETF华夏", security_type=SecurityType.ETF, amount=100_000_000, ch60=45, day=6.0),
+        sec("512000", name="券商ETF", security_type=SecurityType.ETF, amount=300_000_000, ch60=10),
+    ]
+    store = {}
+    add_fund_candidates(store, funds, cap=10, score_floor=0)
+    out = {x.code: x for x in finalize_candidates(store)}
+    assert "510300" in out
+    assert "510310" not in out
+    assert out["510300"].fund_liquidity_percentile is not None
+
+
 def test_funds_are_compared_inside_asset_categories_not_one_mixed_ranking():
     funds = [
         sec("510300", name="沪深300ETF", security_type=SecurityType.ETF, amount=100_000_000, ch60=8),
@@ -103,14 +118,22 @@ def test_funds_are_compared_inside_asset_categories_not_one_mixed_ranking():
     assert {c.value for c in (FundCategory.EQUITY_BROAD, FundCategory.BOND, FundCategory.COMMODITY, FundCategory.CROSS_BORDER)} <= categories
 
 
-def test_lofs_are_classified_by_underlying_asset_instead_of_all_other():
+def test_lofs_are_classified_by_underlying_asset_and_risk_tags_are_separate():
     samples = [
         (sec("160216", name="国泰商品LOF", security_type=SecurityType.LOF), FundCategory.OTHER),
         (sec("160723", name="嘉实原油LOF", security_type=SecurityType.LOF), FundCategory.COMMODITY),
-        (sec("161125", name="标普油气LOF", security_type=SecurityType.LOF), FundCategory.CROSS_BORDER),
+        (sec("161125", name="标普油气LOF", security_type=SecurityType.LOF), FundCategory.COMMODITY),
         (sec("161716", name="招商双债LOF", security_type=SecurityType.LOF), FundCategory.BOND),
         (sec("161725", name="招商白酒LOF", security_type=SecurityType.LOF), FundCategory.EQUITY_SECTOR),
         (sec("164906", name="海外科技LOF", security_type=SecurityType.LOF), FundCategory.CROSS_BORDER),
     ]
     for item, expected in samples:
         assert classify_fund_category(item) is expected
+        assert "LOF_PREMIUM" in fund_risk_tags(item)
+
+    oil_qdii = sec("161125", name="标普油气LOF", security_type=SecurityType.LOF)
+    assert "CROSS_BORDER_QDII" in fund_risk_tags(oil_qdii)
+
+    bond_qdii = sec("160140", name="美元债QDII-LOF", security_type=SecurityType.LOF)
+    assert classify_fund_category(bond_qdii) is FundCategory.BOND
+    assert set(fund_risk_tags(bond_qdii)) == {"CROSS_BORDER_QDII", "LOF_PREMIUM"}
